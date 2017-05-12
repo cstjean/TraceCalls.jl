8a51d81f276bf54522e7ba52a844f85bf6be7ea2
Cedric St-Jean <cedric.stjean@gmail.com>
2017-05-12 11:13:48 -0400

    Introcued custom printing

--- src/TraceCalls.jl
+++ src/TraceCalls.jl
@@ -5,17 +5,17 @@ using Unrolled
 
 export @traceable, @trace
 
-@qmutable Trace(fname, args, kwargs, called::Vector{Trace}, return_value)
+@qmutable Trace(func, args, kwargs, called::Vector{Trace}, return_value)
 
 function Base.copy!(dest::Trace, src::Trace)
-    dest.fname = src.fname
+    dest.func = src.func
     dest.args = src.args
     dest.kwargs = src.kwargs
     dest.called = src.called
     dest.return_value = src.return_value
 end
 
-Base.copy(tr::Trace) = Trace(tr.fname, tr.args, tr.kwargs, tr.called, tr.return_value)
+Base.copy(tr::Trace) = Trace(tr.func, tr.args, tr.kwargs, tr.called, tr.return_value)
 @qstruct NotReturned()
 Base.push!(tr::Trace, sub_trace::Trace) = push!(tr.called, sub_trace)
 
@@ -25,7 +25,7 @@ const trace_data = top_trace()
 const current_trace = fill(trace_data)
 
 macro traceable(fdef)
-    fname, args, kwargs, body_block, ret_type = parse_function_definition(fdef)
+    func, args, kwargs, body_block, ret_type = parse_function_definition(fdef)
     arg_name = Unrolled.function_argument_name
     all_args = map(arg_name, [args..., kwargs...])
     do_body, new_trace, prev_trace = gensym(), gensym(), gensym()
@@ -33,11 +33,11 @@ macro traceable(fdef)
         @inline function $do_body($(all_args...))
             $body_block
         end
-        function $fname($(args...); $(kwargs...))::$ret_type
+        function $func($(args...); $(kwargs...))::$ret_type
             if $TraceCalls.is_tracing[]
                 $prev_trace = $TraceCalls.current_trace[]
                 $new_trace =
-                 $TraceCalls.Trace($(Expr(:quote, fname)), ($(map(arg_name, args)...),),
+                 $TraceCalls.Trace($(Expr(:quote, func)), ($(map(arg_name, args)...),),
                                    ($([:($(Expr(:quote, arg_name(kwa)))=>$(arg_name(kwa)))
                                        for kwa in kwargs]...),),
                                    [], $TraceCalls.NotReturned())
@@ -73,15 +73,23 @@ end
 
 ################################################################################
 
+val_html(x) = string(x)
+
 Base.show(io::IO, ::MIME"text/html", tr::Trace) =
     write(io, "<pre>"*trace_html(tr)*"</pre>")
 
-kwa_eql(kwarg::Pair) = "$(first(kwarg))=$(second(kwarg))"
+kwa_eql(kwarg::Pair) = "$(first(kwarg))=$(val_html(second(kwarg)))"
 kwargs_html(kwargs) = "; " * join(map(kwa_eql, kwargs), ", ")
+args_html(kwargs) = join(map(val_html, kwargs), ", ")
 kwargs_html(kwargs::Tuple{}) = ""
-trace_html(tr::Trace) = 
+sub_called_html(tr::Trace) =
+    (isempty(tr.called) ? "" :
+     "<blockquote>" * mapreduce(trace_html, *, "", tr.called) * "</blockquote>")
+call_html(::Any, tr::Trace) =
     # Could use CSS https://www.computerhope.com/issues/ch001034.htm
-    "$(tr.fname)($(tr.args...)$(kwargs_html(tr.kwargs))) = $(tr.return_value)<br><blockquote>" * mapreduce(trace_html, *, "", tr.called) * "</blockquote>"
+    "$(tr.func)($(args_html(tr.args))$(kwargs_html(tr.kwargs))) = $(val_html(tr.return_value))<br>" * sub_called_html(tr)
+
+trace_html(tr::Trace) = call_html(tr.func, tr)
 
 
 end # module
